e80cfd1644425e8ae912f76cf60d3e0e00ff4ebf
Mahmoud Ramadan <Mahmoud Abdallah>
2020-11-08 13:58:04 +0200

    created a simple program to check if a word is a palindrome

---
+++ task_21_CheckIfAWordIsPalindrome/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[1000];
+    int i,n,c=0;
+
+    printf("Enter  the string : ");
+    // get the input from the user
+    gets(s);
+    // get the length of the input
+    n=strlen(s);
+    // iteration
+    for(i=0;i<n/2;i++)
+    {
+    	if(s[i]==s[n-i-1])
+    	c++;
+
+ 	}
+
+ 	if(c==i)
+ 	    printf("string is palindrome");
+    else
+        printf("string is not palindrome");
+
+    return 0;
+}
